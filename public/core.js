e4d9abf02b06ffa23f12b9cff89be463c553946a
LSK <sinrim10@naver.com>
2015-06-25 22:29:38 +0900

    test

---
+++ public/core.js
@@ -0,0 +1,51 @@
+
+var lskTodo = angular.module('lskTodo', []);
+//앵귤러 컨트롤러
+function mainController($scope, $http) {
+    $scope.formData = {};
+    $scope.error = "";
+    //get 할일 조회
+    $http.get('/api/todos')
+        .success(function(data) {
+            $scope.todos = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    //post 할일 생성
+    $scope.createTodo = function() {
+        console.log("aa : " + $scope.formData.text);
+
+        if($scope.formData.text === undefined || $scope.formData.text === ""){
+            //alert('입력 하세요');
+            $scope.error = '할일을 입력하세요';
+            return;
+        } else{
+            $scope.error = '';
+        }
+        $http.post('/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {};
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    //delete 할일 삭제
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/todos/' + id)
+            .success(function(data) {
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+}
