5e77607b976ee4e04516201d874c5991d7bb8068
jms <kkforgg@naver.com>
2015-09-14 23:28:03 +0900

    다시 변경

--- index.js
+++ index.js
@@ -4,43 +4,22 @@ var morgan = require('morgan');             // 리퀘스트 로그
 var bodyParser = require('body-parser');    // html 정보
 var methodOverride = require('method-override'); // delete,put
 var http = require('http');
-
+var app = express();
 var todoCtrl = require('./controller/todoController');
-var todo = require('./model/todo');
+
+
 
 // routes ======================================================================
 
-var app = express();
+
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));                                         // 모든 요청 로그
 app.use(bodyParser.json());                                     // application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // application/vnd.api+json as json
 app.use(methodOverride());
 
-
-//모든 데이터 조회
-app.get('/api/todos', function(req, res) {
-
-	todo.init(req,res);
-});
-
-//post 요청 처리
-app.post('/api/todos', function(req, res) {
-
-	todo.todoInsert(req,res);
-});
-
-//검색
-app.get('/api/todos/:search', function(req, res) {
-	todo.search(req,res);
-});
-
-
-//delete 처리 리스트 삭제
-app.delete('/api/todos/:todo_id', function(req, res) {
-	todo.todoDetele(req,res);
-});
-
+// Todo 컨트롤러
+todoCtrl.todoCtrl(app);
 
 
 // 서버 스타트...
--- model/todo.js
+++ model/todo.js
@@ -1,8 +1,4 @@
-
-/**  디비 커넥션 설정 */
-var mongoose = require('mongoose');                     // 몽고디비 모듈
-mongoose.connect('mongodb://localhost:27017/todolist');     // 몽고디비연결
-/** 설정 끝 */
+var mongoose = require('../db');
 
 // define model =================
 var Todo = mongoose.model('Todo', {
